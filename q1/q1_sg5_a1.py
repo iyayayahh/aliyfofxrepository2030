68d435acb987efe8044eccbb3ef9954737706289
iyayayahh <amcabanayan2030@clc.pshs.edu.ph>
2026-08-27 09:05:34 +0800

    Add files via upload

---
+++ q1/q1_sg5_a1.py
@@ -0,0 +1,17 @@
+#The RPG Hero Game
+
+class Hero:
+    def __init__(self, name, hp = 100):
+        self.name = name
+        self.hp = hp
+    def take_damage(self, amount):
+        self.hp -= amount
+
+arthur = Hero ("Arthur", 100)
+morgana = Hero ("Morgana", 100)
+
+arthur.take_damage(10)
+
+print (f"{arthur.name}'s HP: {arthur.hp} after Morgana used her Phoenix Strike!")
+print (f"{morgana.name}'s HP: {morgana.hp}")
+
